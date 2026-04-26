c2440625ad08f3303a89333964e4d2ab1446d98c
C. Reyes <cvreyes@hotmail.com>
2026-04-26 17:16:11 +0800

    Snake Game Scoreboard

--- day-20/snake-game/main.py
+++ day-20/snake-game/main.py
@@ -1,6 +1,8 @@
 import time
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -19,9 +23,13 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
-
+    time.sleep(0.5)
     snake.move()
+
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 
 screen = Screen()
 screen.exitonclick()
