0e36f1d4c34cd05b16b976461cd654d9e2a5b184
C.Reyes <cvreyes@hotmail.com>
2026-05-08 15:00:59 +0800

    Day 33 - Until Matching Sunsets

---
+++ day-33-MatchSunsetTimes/main.py
@@ -0,0 +1,21 @@
+import requests
+from datetime import datetime
+
+parameters = {
+    "lat": 14.676041,
+    "lng": 121.043701,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
+
