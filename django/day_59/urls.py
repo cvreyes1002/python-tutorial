ec5447feaf3d1bb5ae180c7754fc0e955d18963b
C.Reyes <cvreyes@hotmail.com>
2026-05-20 15:22:33 +0800

    Finish Day 60

--- django/day_59/urls.py
+++ django/day_59/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.main_page),
     path("show_post/<int:index>", views.show_post, name="show-post"),
     path("about", views.about),
-    path("contact", views.contact),
+    path("contact", views.contact, name="contact"),
 ]
--- django/day_59/views.py
+++ django/day_59/views.py
@@ -5,6 +5,8 @@ import requests
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 # Create your views here.
+
+
 def main_page(request):
     print(posts)
     return render(request, "day_59/index.html", {
@@ -23,10 +25,22 @@ def show_post(request, index):
     })
 
 # @app.route("/about")
+
+
 def about(request):
     return render(request, "day_59/about.html")
-
-
 # @app.route("/contact")
+
+
 def contact(request):
-    return render(request, "day_59/contact.html")
+    if request.method == "POST":
+        data = request.POST
+        # data = request.form
+        print(data)
+        # send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render(request, "day_59/contact.html", {
+            "msg_sent": True,
+        })
+    return render(request, "day_59/contact.html", {
+        "msg_sent": False,
+    })
